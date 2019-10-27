340d7451d45eacd416c47f34a3ca52ffaee41c19
Edidiong <developer1@theagromall.com>
2019-10-27 11:57:27 +0100

    second commit

--- app/src/main/java/com/example/android/live_track/MainActivity.java
+++ app/src/main/java/com/example/android/live_track/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.android.live_track;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.raycoarana.codeinputview.CodeInputView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_verify);
+
+
+    }
+
+    public void verify(View view){
+
+        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+        CodeInputView codeInputView = findViewById(R.id.input_code);
+        startActivity(intent);
+
+
     }
 }
