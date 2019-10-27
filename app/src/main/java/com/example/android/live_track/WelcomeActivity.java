04fc3ee4c5669f1583a97a81f372e30c98224bf1
Edidiong <developer1@theagromall.com>
2019-10-27 16:28:42 +0100

    just commit

--- app/src/main/java/com/example/android/live_track/WelcomeActivity.java
+++ app/src/main/java/com/example/android/live_track/WelcomeActivity.java
@@ -10,32 +10,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WelcomeActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    Switch simpleSwitch;
-    TextView text;
-    TextView text2;
+import com.suke.widget.SwitchButton;
+
+public class WelcomeActivity extends AppCompatActivity  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        simpleSwitch =  findViewById(R.id.switches);
-        text = findViewById(R.id.switch2);
-        text2 = findViewById(R.id.switch1);
-
-        if (simpleSwitch != null) {
-            simpleSwitch.setOnCheckedChangeListener(this);
-        }
-        setContentView(R.layout.activity_welcome);
+     setContentView(R.layout.activity_welcome);
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked) {
-            text.setVisibility(View.VISIBLE);
-            text2.setVisibility(View.GONE);
-
-        } else {
-            text2.setVisibility(View.VISIBLE);
-            text.setVisibility(View.GONE);
-        }
-    }
 }
