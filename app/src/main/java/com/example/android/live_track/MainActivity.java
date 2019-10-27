3b8724e1a9a36fc2c61d08d8d971ac8789c6fb80
Edidiong <developer1@theagromall.com>
2019-10-27 15:29:03 +0100

    extra commit

--- app/src/main/java/com/example/android/live_track/MainActivity.java
+++ app/src/main/java/com/example/android/live_track/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.android.live_track;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
+    EditText editText5;
+    EditText editText6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_verify);
+        editText1 = findViewById(R.id.code1);
+        editText2 = findViewById(R.id.code2);
+        editText3 = findViewById(R.id.code3);
+        editText4 = findViewById(R.id.code4);
+        editText5 = findViewById(R.id.code5);
+        editText6 = findViewById(R.id.code6);
+        if (editText1.length()==1) {
+            editText2.requestFocus();
+        }
     }
+
+
+    public void verify(View view) {
+        Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/android/live_track/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.live_track;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    Switch simpleSwitch;
+    TextView text;
+    TextView text2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        simpleSwitch =  findViewById(R.id.switches);
+        text =findViewById(R.id.switch2);
+        text2 =findViewById(R.id.switch1);
+
+
+        if (simpleSwitch != null) {
+            simpleSwitch.setOnCheckedChangeListener(this);
+        }
+        setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked) {
+            text.setVisibility(View.VISIBLE);
+            Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+            text2.setVisibility(View.GONE);
+
+        } else {
+            text2.setVisibility(View.VISIBLE);
+            text.setVisibility(View.GONE);
+        }
+    }
+}
