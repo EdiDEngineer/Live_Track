a1eb58af46fe9e6b2a3ba3dc9c104f31721cfb95
Edidiong <developer1@theagromall.com>
2019-10-29 10:23:40 +0100

    Join just UI

--- app/src/main/java/com/example/android/live_track/MainActivity.java
+++ app/src/main/java/com/example/android/live_track/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.android.live_track;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.raycoarana.codeinputview.CodeInputView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
-        EditText editText1 = findViewById(R.id.code1);
-       /* final EditText editText2 = findViewById(R.id.code2);
-        EditText editText3 = findViewById(R.id.code3);
-        EditText editText4 = findViewById(R.id.code4);
-        EditText editText5 = findViewById(R.id.code5);
-        EditText editText6 = findViewById(R.id.code6);*/
-//editText1.length()
-        editText1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean hasFocus) {
-               // editText2.requestFocus();
-            }
-        });
-
 
     }
 
-    public void verify(View view){
+
+    public void verify(View view) {
         Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
         startActivity(intent);
     }
+
+
+
 }
--- app/src/main/java/com/example/android/live_track/WelcomeActivity.java
+++ app/src/main/java/com/example/android/live_track/WelcomeActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.live_track;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+
+
 }
