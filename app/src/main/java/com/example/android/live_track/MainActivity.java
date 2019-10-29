169bae84151d7ee3eb3f22dc0274b982c06e0f4d
Edidiong <developer1@theagromall.com>
2019-10-29 11:55:58 +0100

    next commit

--- app/src/main/java/com/example/android/live_track/MainActivity.java
+++ app/src/main/java/com/example/android/live_track/MainActivity.java
@@ -2,28 +2,29 @@ package com.example.android.live_track;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.poovam.pinedittextfield.PinField;
+
 public class MainActivity extends AppCompatActivity {
+    PinField pinField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
-
+        pinField = findViewById(R.id.pin);
     }
-
 
     public void verify(View view) {
         Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
-        startActivity(intent);
+        intent.putExtra("EXTRA_SESSION", pinField.getText().toString());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
-
 
 
 }
--- app/src/main/java/com/example/android/live_track/WelcomeActivity.java
+++ app/src/main/java/com/example/android/live_track/WelcomeActivity.java
@@ -1,22 +1,38 @@
 package com.example.android.live_track;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WelcomeActivity extends AppCompatActivity  {
+import com.suke.widget.SwitchButton;
+
+public class WelcomeActivity extends AppCompatActivity {
+    TextView key;
+    TextView status;
+    SwitchButton switchbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        key = findViewById(R.id.key_value);
+        switchbutton = findViewById(R.id.switch1);
+        status = findViewById(R.id.status);
+        key.setText(getIntent().getStringExtra("EXTRA_SESSION"));
+
+        switchbutton.setOnCheckedChangeListener(
+                new SwitchButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                        if (isChecked) {
+                            status.setText("Tracking ON");
+                        } else {
+                            status.setText("Tracking OFF");
+                        }
+
+                    }
+                });
     }
-
-
 }
